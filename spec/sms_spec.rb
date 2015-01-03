14312b8c60bcfa29b8f2614e9c3f8f1dde2a4c49
amassaad <alex.massaad@shopify.com>
2015-01-03 13:31:14 -0500

    Save the day

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -5,7 +5,6 @@ require 'timecop'
 set :environment, :test
 welcome_response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Gather numDigits=\"1\" action=\"/in-call/get\" method=\"post\"><Say>Welcome to York Street. Deliveries, please press 5.\n        For a joke, press 1.\n        To speak to a person, press 2.\n        To check your future, press 3.\n        If you know anything else, at all. Please enter it now!</Say></Gather><Say>Sorry, I didn't get your response</Say><Redirect>https://york-phone-gateway.herokuapp.com/in-call</Redirect></Response>"
 
-root = ""
 describe 'SMS and Call Response Capabilities' do 
 	include Rack::Test::Methods
 
