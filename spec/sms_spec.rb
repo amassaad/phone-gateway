66592b90cbda2f5ff6571e803c2986859b9404ec
Alex Massaad <alex.massaad@shopify.com>
2015-11-03 19:07:50 -0500

    return

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -3,8 +3,7 @@ require 'rack/test'
 require 'timecop'
 
 set :environment, :test
-welcome_response1 = "
-<?xml version="1.0" encoding="UTF-8"?><Response><Gather numDigits="1" action="/in-call/get" method="post"><Play>https://s3-us-west-2.amazonaws.com/yorkphonegateway/welcome_to_york.wav</Play></Gather><Say>Sorry, I didn't get your response</Say><Redirect>https://york-phone-gateway.herokuapp.com/in-call</Redirect></Response>"
+welcome_response1 = "<?xml version="1.0" encoding="UTF-8"?><Response><Gather numDigits="1" action="/in-call/get" method="post"><Play>https://s3-us-west-2.amazonaws.com/yorkphonegateway/welcome_to_york.wav</Play></Gather><Say>Sorry, I didn't get your response</Say><Redirect>https://york-phone-gateway.herokuapp.com/in-call</Redirect></Response>"
 welcome_response = "<Say>Welcome to York Street. Deliveries, please press 1."
 
 describe 'SMS and Call Response Handler' do
