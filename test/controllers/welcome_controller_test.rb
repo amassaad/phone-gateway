f335bf7717c44aa8f41796092f9fadd90432e5ff
Alex Massaad <alex.massaad@shopify.com>
2017-03-12 13:30:54 -0400

    Welcome statsd

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
-    get root_url
+    assert_statsd_gauge('welcomecontroller.bamboo') do
+      get root_url
+    end
     assert_response :success
   end
 end
